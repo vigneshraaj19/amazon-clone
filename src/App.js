f6276863079070af9cf91de29e1357bebc30f13f
vigneshraaj <vigneshraaj19@gmail.com>
2022-05-27 12:24:11 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello vigneshraaj 😂😊</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA3LE00Fm4W5aUCGVOdluXmyG8FHcGHgaA",
+    authDomain: "challenge-113d7.firebaseapp.com",
+    projectId: "challenge-113d7",
+    storageBucket: "challenge-113d7.appspot.com",
+    messagingSenderId: "716600883509",
+    appId: "1:716600883509:web:ad31c70162efe77403747c",
+    measurementId: "G-65W6VZETPB"
+  };
