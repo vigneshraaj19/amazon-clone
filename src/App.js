23dbdb1679e39974ebca85de58d1bb54548cb4c4
vigneshraaj <vigneshraaj19@gmail.com>
2022-05-28 16:01:48 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Header from './Header';
 import Home from './Home'
+import './index.js';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+    <Routes>
+
+      <Route path="/" element={<><Header/><Home/></>}  />
+      <Route path="/checkout" element={<><Header/><Checkout /></>} />
+      
+    </Routes>
     </div>
+
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import './Product.css';
 
-function Product() {
+function Product({title,image,price,rating }) {
   return (
     <div className="product">
-        <div className="product__info">
-            <p>India Today English </p>
-            <p className='product__price'>
-                <small>$</small>
-                <strong>19.99</strong>
-            </p>
-            <div className="product__rating">
-                <p>⭐⭐⭐⭐⭐</p>
-            </div>
-        </div>
-        <img src="https://m.media-amazon.com/images/I/51SICavnBKL._AC_UL320_.jpg"></img>
-        <button>Add to Basket</button>
+    <div className="product__info">
+      <p>{title}</p>
+      <p className="product__price">
+        <small>$</small>
+        <strong>{price}</strong>
+      </p>
+      <div className="product__rating">
+        {Array(rating)
+          .fill()
+          .map((item) => (
+            <p>⭐</p>
+          ))}
+      </div>
     </div>
+
+    <img src={image} alt="" />
+    <button>Add to basket</button>
+
+  </div>
   )
 }
 
