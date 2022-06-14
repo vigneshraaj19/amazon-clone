8f8e99628dff3e093d587269dcb03dcfe02d8937
vigneshraaj <vigneshraaj19@gmail.com>
2022-06-14 14:43:28 +0530

    swiper

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,21 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
 export default () => {
   return (
 <Swiper
 // install Swiper modules
-modules={[Navigation]}
+modules={[Navigation,Autoplay]}
 slidesPerView={1}
+loop={true}
+autoplay={{
+    delay: 1000,
+}}
 navigation
 >
    <SwiperSlide>
